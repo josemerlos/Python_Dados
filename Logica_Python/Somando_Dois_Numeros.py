599ce9f94a58ec8d8abe8e4510b4e9f0c2d15aca
Tiponcio <j.merlosjunior@dxc.com>
2022-03-30 22:04:51 -0300

    Um exercicio adicionado

---
+++ Logica_Python/Somando_Dois_Numeros.py
@@ -0,0 +1,11 @@
+
+
+
+valor1 = input("Digite um valor: ")
+valor2 = input("Digite o outro valor: ")
+
+resultado = float(valor1) + float(valor2)
+
+print('A soma entre ', valor1 , 'e ', valor2 , 'é ', resultado)
+
+
